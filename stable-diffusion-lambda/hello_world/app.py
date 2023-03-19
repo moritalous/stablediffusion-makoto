eb96e07baf0f33ad6fd387476b2a70447bcfeca5
moritalous <moritalous@gmail.com>
2023-03-19 09:21:40 +0000

    update

--- stable-diffusion-lambda/hello_world/app.py
+++ stable-diffusion-lambda/hello_world/app.py
@@ -33,28 +33,33 @@ def lambda_handler(event, context):
     img_bin = base64.b64decode(event['body'])
     init_image = Image.open(BytesIO(img_bin))
 
-    convert_image = {
-        1: lambda img: img,
-        # 左右反転
-        2: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT),
-        # 180度回転
-        3: lambda img: img.transpose(Image.ROTATE_180),
-        # 上下反転
-        4: lambda img: img.transpose(Image.FLIP_TOP_BOTTOM),
-        # 左右反転＆反時計回りに90度回転
-        5: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_90),
-        # 反時計回りに270度回転
-        6: lambda img: img.transpose(Image.ROTATE_270),
-        # 左右反転＆反時計回りに270度回転
-        7: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_270),
-        # 反時計回りに90度回転
-        8: lambda img: img.transpose(Image.ROTATE_90),
-    }
+    try:
+        convert_image = {
+            1: lambda img: img,
+            # 左右反転
+            2: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT),
+            # 180度回転
+            3: lambda img: img.transpose(Image.ROTATE_180),
+            # 上下反転
+            4: lambda img: img.transpose(Image.FLIP_TOP_BOTTOM),
+            # 左右反転＆反時計回りに90度回転
+            5: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_90),
+            # 反時計回りに270度回転
+            6: lambda img: img.transpose(Image.ROTATE_270),
+            # 左右反転＆反時計回りに270度回転
+            7: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_270),
+            # 反時計回りに90度回転
+            8: lambda img: img.transpose(Image.ROTATE_90),
+        }
 
-    exif = init_image._getexif()
-    orientation = exif.get(0x112, 1)
-    init_image = convert_image[orientation](init_image).convert('RGB')
+        exif = init_image._getexif()
+        orientation = exif.get(0x112, 1)
+        init_image = convert_image[orientation](init_image)
+    except:
+        pass
 
+    init_image = init_image..convert('RGB')
+    
     init_width = init_image.width
     init_height = init_image.height
 
